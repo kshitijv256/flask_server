2e9828fbd1f958e216f03266d590db28f3127fc6
Kshitij Verma <kshitijv250@gmail.com>
2023-07-31 16:42:51 +0530

    switched to mobilenet model

---
+++ main.py
@@ -0,0 +1,96 @@
+import os
+import glob
+from flask import Flask, flash, request, redirect, url_for
+
+# from flask import send_from_directory
+from werkzeug.utils import secure_filename
+
+import solution
+
+##
+with open("views/main.html", "r") as f:
+    mainPage = f.read()
+with open("views/results.html", "r") as f:
+    resultsPage = f.read()
+##
+
+UPLOAD_FOLDER = "static/"
+ALLOWED_EXTENSIONS = {"png", "jpg", "jpeg", "gif", ""}
+
+app = Flask(__name__)
+app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
+app.secret_key = "super_secret"
+
+
+def allowed_file(filename):
+    return "." in filename and filename.rsplit(".", 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+@app.route("/", methods=["GET", "POST"])
+def upload_file():
+    if request.method == "POST":
+        # check if the post request has the file part
+        if "file" not in request.files:
+            flash("No file part")
+            return redirect(request.url)
+        file = request.files["file"]
+        # If the user does not select a file, the browser submits an
+        # empty file without a filename.
+        if file.filename == "":
+            flash("No selected file")
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config["UPLOAD_FOLDER"], filename))
+            files = glob.glob("./contours/*")
+            for f in files:
+                os.remove(f)
+            # return redirect(url_for('results', name=filename))
+            model = solution.loadModel()
+            img = solution.loadImg(os.path.join(app.config["UPLOAD_FOLDER"], filename))
+            detectedList = solution.findContours(img)
+            pred, result = solution.predictResult(detectedList, model)
+
+            if result == None:
+                print(pred)
+                return "No result"
+            else:
+                print(pred + " : " + str(result))
+                return str(result)
+    return mainPage
+
+
+@app.route("/results/<name>")
+def results(name):
+    # return send_from_directory(app.config['UPLOAD_FOLDER'], name)
+
+    labelEncoder = solution.prepareEncoder()
+    model = solution.loadModel()
+    img = solution.loadImg(os.path.join(app.config["UPLOAD_FOLDER"], name))
+    detectedList = solution.findContours(img)
+    pred, result = solution.predictResult(model, detectedList, labelEncoder)
+
+    resultsPageX = resultsPage.replace("imageName", name)
+
+    if result is None:
+        print(pred)
+        st = """
+        <p class="p-4 py-2 font-semibold text-xl text-red-400">~~Unclear Text~~</p>
+        <p class="p-4 py-2 font-bold text-xl text-red-400">Please try again!!</p>"""
+        return resultsPageX.replace("replaceit", st)
+    else:
+        print(pred + " = " + str(result))
+        st = """
+        <p class="p-4 font-semibold text-xl">Solved successfully!</p>
+        <p class="p-2 mx-3">Results:</p>
+        <p class="p-2 mx-3 font-bold text-xl text-lime-500">{}</p>
+        """.format(
+            pred + " = " + str(result)
+        )
+        return resultsPageX.replace("replaceit", st)
+
+
+app.add_url_rule("/results/<name>", "results", build_only=True)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
--- solution.py
+++ solution.py
@@ -1,4 +1,4 @@
-'''
+"""
 @ on 4/14/2022 by kshitijv256
 Driver file for input image processing and result prediction
 
@@ -8,24 +8,32 @@ Updated the code to use CNN model for prediction
 @ on 11/02/2022 by kshitijv256
 Updated the code to be use in Flask app
 
-'''
+"""
 import cv2
 import os
 import numpy as np
 from keras.models import load_model
 import pickle
+from tensorflow.keras.optimizers import Adamax
 
 
 ## preparing
 def prepareEncoder():
-    with open('./Assets/models/encoder.pickle', 'rb') as handle:
+    with open("./Assets/models/encoder.pickle", "rb") as handle:
         label_encoder = pickle.load(handle)
     return label_encoder
 
+
 # Load model
 def loadModel():
-    model = load_model('./Assets/models/new_model_3.h5')
+    model = load_model("./Assets/models/modelv3.h5", compile=False)
+    model.compile(
+        Adamax(learning_rate=0.001),
+        loss="categorical_crossentropy",
+        metrics=["accuracy"],
+    )
     return model
+
 
 # Load Image
 def loadImg(path):
@@ -33,108 +41,150 @@ def loadImg(path):
     img = cv2.cvtColor(y, cv2.COLOR_BGR2GRAY)
     return img
 
+
 # Find contours
 def findContours(img):
     if img is not None:
-        img=~img
-        ret,thresh=cv2.threshold(img,140,255,0)
-        ctrs,ret=cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_NONE)
-        cnt=sorted(ctrs, key=lambda ctr: cv2.boundingRect(ctr)[0])
+        img = ~img
+        ret, thresh = cv2.threshold(img, 120, 255, 0)
+        ctrs, ret = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+        cnt = sorted(ctrs, key=lambda ctr: cv2.boundingRect(ctr)[0])
 
-        w=int(45)
-        h=int(45)
-        testset=[]
+        w = int(128)
+        h = int(128)
+        testset = []
 
-        rects=[]
-        for c in cnt :
-            x,y,w,h= cv2.boundingRect(c)
-            rect=[x,y,w,h]
+        rects = []
+        for c in cnt:
+            x, y, w, h = cv2.boundingRect(c)
+            rect = [x - 5, y - 10, w + 10, h + 20]
             rects.append(rect)
 
-        bool_rect=[]
+        bool_rect = []
         for r in rects:
-            l=[]
+            l = []
             for rec in rects:
-                flag=0
-                if rec!=r:
-                    if r[0]<(rec[0]+rec[2]+10) and rec[0]<(r[0]+r[2]+10) and r[1]<(rec[1]+rec[3]+10) and rec[1]<(r[1]+r[3]+10):
-                        flag=1
+                flag = 0
+                if rec != r:
+                    if (
+                        r[0] < (rec[0] + rec[2] + 10)
+                        and rec[0] < (r[0] + r[2] + 10)
+                        and r[1] < (rec[1] + rec[3] + 10)
+                        and rec[1] < (r[1] + r[3] + 10)
+                    ):
+                        flag = 1
                     l.append(flag)
-                if rec==r:
+                if rec == r:
                     l.append(0)
             bool_rect.append(l)
 
-        dump_rect=[]
-        for i in range(0,len(cnt)):
-            for j in range(0,len(cnt)):
-                if bool_rect[i][j]==1:
-                    area1=rects[i][2]*rects[i][3]
-                    area2=rects[j][2]*rects[j][3]
-                    if(area1==min(area1,area2)):
+        dump_rect = []
+        for i in range(0, len(cnt)):
+            for j in range(0, len(cnt)):
+                if bool_rect[i][j] == 1:
+                    area1 = rects[i][2] * rects[i][3]
+                    area2 = rects[j][2] * rects[j][3]
+                    if area1 == min(area1, area2):
                         dump_rect.append(rects[i])
 
-        final_rect=[i for i in rects if i not in dump_rect]
+        final_rect = [i for i in rects if i not in dump_rect]
         count = 1
         for r in final_rect:
-            x=r[0]
-            y=r[1]
-            w=r[2]
-            h=r[3]
-            im_crop =thresh[y:y+h+10,x:x+w+10]
+            x = r[0]
+            y = r[1]
+            w = r[2]
+            h = r[3]
+            if w < 50 and h < 50:
+                continue
+            h = max(h, w)
+            w = max(h, w)
+            im_crop = thresh[y - h // 2 : y + h + 10, x : x + w + 10]
 
-            height, width = im_crop.shape
-            x = height if height > width else width
-            y = height if height > width else width
+            # height, width = im_crop.shape
+            # x = height if height > width else width
+            # y = height if height > width else width
 
-            ## To make contours square
-            square= np.zeros((x,y), np.uint8)
-            square[int((y-height)/2):int(y-(y-height)/2), int((x-width)/2):int(x-(x-width)/2)] = im_crop
-            im_crop = square
+            # ## To make contours square
+            # square = np.zeros((x, y), np.uint8)
+            # square[
+            #     int((y - height) / 2) : int(y - (y - height) / 2),
+            #     int((x - width) / 2) : int(x - (x - width) / 2),
+            # ] = im_crop
+            # im_crop = square
 
             ## To resize image
-            img = cv2.resize(im_crop,(45,45))
-            # img = cv2.blur(img,(1,1))
-            img=~img
-            cv2.imwrite('contours/contours{}.jpg'.format(count), img)
-            count+=1
-
-
+            img = cv2.resize(im_crop, (96, 96))
+            img = ~img
+            # img = cv2.blur(img, (5, 5))
+            cv2.imwrite("./contours/contours{}.jpg".format(count), img)
+            count += 1
             img = np.array(img)
             img = np.expand_dims(img, axis=0)
-            stacked_img = np.stack((img,)*3, axis=-1)
+            stacked_img = np.stack((img,) * 3, axis=-1)
             testset.append(stacked_img)
         testset = np.array(testset)
-        testset = testset.astype('float32')
-        testset /= 255
         return testset
     return None
 
+
 # Predict
-def predictResult(model, testset, label_encoder):
-    s=''
-    for i in range(len(testset)):
-        pred=model.predict(testset[i])
-        result = np.argsort(pred)
-        result_temp = result[0][::-1]
-        # print(result_temp)
+def predictValues(features, model):
+    res = []
+    for i in range(len(features)):
+        image = features[i]
+        image = image / 255.0
+        prediction = model.predict(image)
+        value = np.argmax(prediction)
+        res.append(value)
+        print("Prediction is {}.".format(value))
+    return res
 
-        result = label_encoder.inverse_transform(np.array(result_temp))
 
-        if(result[0]=='div' or result[0]=='forward_slash'):
-            s=s+'/'
-        elif(result[0]=='times'):
-            s=s+'*'
-        ## NEW CODE
-        elif(result[0]=='(' or result[0]=='{' or result[0]=='['):
-            s=s+'('
-        elif(result[0]==')' or result[0]=='}' or result[0]==']'):
-            s=s+')'
-        elif(result[0]=='='):
-            s=s+'-'
-        else:
-            s=s+result[0]
+def predictResult(result, model):
+    result = predictValues(result, model)
+    equation = ""
+    for i in range(len(result)):
+        if result[i] < 10:
+            equation = equation + str(result[i])
+        if result[i] == 10:
+            equation = equation + "+"
+        if result[i] == 11:
+            equation = equation + "."
+        if result[i] == 12:
+            equation = equation + "/"
+        if result[i] == 13:
+            equation = equation + "*"
+        if result[i] == 14:
+            equation = equation + "-"
     try:
-        x = eval(s)
-        return s,x
+        out = eval(equation)
     except:
-        return s, None
+        out = "Incorrect equation"
+    return equation, out
+    # s = ""
+    # for i in range(len(testset)):
+    #     pred = model.predict(testset[i])
+    #     result = np.argsort(pred)
+    #     result_temp = result[0][::-1]
+    #     # print(result_temp)
+
+    #     result = label_encoder.inverse_transform(np.array(result_temp))
+
+    #     if result[0] == "div" or result[0] == "forward_slash":
+    #         s = s + "/"
+    #     elif result[0] == "times":
+    #         s = s + "*"
+    #     ## NEW CODE
+    #     elif result[0] == "(" or result[0] == "{" or result[0] == "[":
+    #         s = s + "("
+    #     elif result[0] == ")" or result[0] == "}" or result[0] == "]":
+    #         s = s + ")"
+    #     elif result[0] == "=":
+    #         s = s + "-"
+    #     else:
+    #         s = s + result[0]
+    # try:
+    #     x = eval(s)
+    #     return s, x
+    # except:
+    #     return s, None
